81389a3c7a4685397f491a3925f55751d9c6a489
江越天 <jiangyt@yunker.com.cn>
2020-06-23 23:29:36 +0800

    ..

--- src/main/java/com/vitality/ServerApp.java
+++ src/main/java/com/vitality/ServerApp.java
@@ -1,20 +1,19 @@
 package com.vitality;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-@MapperScan({"com.vitality.material.mapper"})
 public class ServerApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApp.class, args);
+
+		ExcelService excelService = new ExcelService();
+		try {
+			excelService.insertByExcel("/Users/egoshiten/Desktop/aaa");
+		}catch (Exception e){
+
+		}
 	}
-
-
-
-
 }
